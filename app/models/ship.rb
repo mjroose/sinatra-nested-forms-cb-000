252a32bf526ed0bb87995cf1fa39362def806d61
Michael Roose <mjroose@gmail.com>
2018-10-27 17:54:38 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,13 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+  end
+
+  def self.all
+    @@all
+  end
 end
